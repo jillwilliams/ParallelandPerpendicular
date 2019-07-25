0bf1603d9a1479528a2a3e6ca9d698f4843c985a
Jill Williams <codeGeek.03.14@gmail.com>
2019-07-25 12:09:17 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const youDecide = ( ) => {
+    return "Good Job!";
+};
+const myanswer = ( ) => {
+    document.getElementById("demo").innerHTML = youDecide( );
+}
+
+const youDecide1 = ( ) => {
+    return "Good Job!";
+};
+const myanswer1 = ( ) => {
+    document.getElementById("demo").innerHTML = youDecide1( );
+}
+
+const youDecide2 = ( ) => {
+    return "Good Job!";
+};
+const myanswer2 = ( ) => {
+    document.getElementById("demo").innerHTML = youDecide2( );
+}
